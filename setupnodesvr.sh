d2e74ff192b5a0ac705f61f03dced935b3061881
Francis Simy Nazareth <simy@outlook.com>
2021-07-23 00:31:31 +0300

    adding sql server

--- setupnodesvr.sh
+++ setupnodesvr.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-
 apt-get update -y
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt-get install -y npm nodejs
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+add-apt-repository universe
+apt-get -y install apt-transport-https
+apt-get -y update
+apt-get -y install unzip apache2 dotnet-sdk-2.2
 ufw allow 8080/tcp
-node app.js &
+tar -xvf vote.tar
+cd vote
+npm start
