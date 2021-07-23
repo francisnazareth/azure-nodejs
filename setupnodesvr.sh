e067dbd43fdc4e7e513b7ef84e7428c5bafa94d4
Francis Simy Nazareth <simy@outlook.com>
2021-07-23 17:21:20 +0300

    fixing errors

--- setupnodesvr.sh
+++ setupnodesvr.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 apt-get update -y
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-apt-get install -y npm nodejs
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
-add-apt-repository universe
-apt-get -y install apt-transport-https
-apt-get -y update
-apt-get -y install unzip
+apt-get install -y nodejs
 ufw allow 8080/tcp
 tar -xvf vote.tar
 cd vote
