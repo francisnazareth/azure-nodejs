2322585d2d870c3df95d8ee7ed78b266748aa2b9
Francis Simy Nazareth <simy@outlook.com>
2021-07-24 01:10:08 +0300

    forget sqlcmd

--- setupnodesvr.sh
+++ setupnodesvr.sh
@@ -3,8 +3,7 @@ apt-get update -y
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
-apt-get install -y nodejs mssql-tools unixodbc-dev
-/opt/mssql-tools/bin/sqlcmd -S $1 -U $2 -P $3 -d sample -i football_table.sql
+apt-get install -y nodejs
 ufw allow 8080/tcp
 tar -xvf vote.tar
 cd vote
