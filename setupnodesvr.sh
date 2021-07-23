db4d2c0b6bf4ec131b6d7e4d1be7383c43999504
Francis Simy Nazareth <simy@outlook.com>
2021-07-23 21:59:08 +0300

    specify db

--- setupnodesvr.sh
+++ setupnodesvr.sh
@@ -4,7 +4,7 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 apt-get install -y nodejs mssql-tools unixodbc-dev
-/opt/mssql-tools/bin/sqlcmd -S $1 -U sqladmin -P Passw0rd!123 -i football_table.sql
+/opt/mssql-tools/bin/sqlcmd -S $1 -U sqladmin -P Passw0rd!123 -d sample -i football_table.sql
 ufw allow 8080/tcp
 tar -xvf vote.tar
 cd vote
