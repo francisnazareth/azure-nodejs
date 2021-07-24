efb1f0d88de8915790741b6ddcd4ffed5beba741
Francis Simy Nazareth <simy@outlook.com>
2021-07-24 20:48:12 +0300

    changing default directory for vote

--- setupnodesvr.sh
+++ setupnodesvr.sh
@@ -3,8 +3,8 @@ apt-get update -y
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 apt-get install -y nodejs
 ufw allow 8080/tcp
-tar -xvf vote.tar
-cd vote
+tar -xf vote.tar -C /home/linadmin/
+cd /home/linadmin/vote
 sed -i "s/DB_HOST_REPLACE_ME/$1/g" .env
 sed -i "s/DB_USER_REPLACE_ME/$2/g" .env
 sed -i "s/DB_PASS_REPLACE_ME/$3/g" .env
