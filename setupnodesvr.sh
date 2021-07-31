baced45575ab41fa8f0bc6fe27fd823313de4e7b
Francis Simy Nazareth <simy@outlook.com>
2021-07-31 20:32:23 +0300

    code for app insights

--- setupnodesvr.sh
+++ setupnodesvr.sh
@@ -3,9 +3,10 @@ apt-get update -y
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 apt-get install -y nodejs
 ufw allow 8080/tcp
-tar -xf vote.tar -C /home/linadmin/
+tar -zxf vote.tgz -C /home/linadmin/
 cd /home/linadmin/vote
 sed -i "s/DB_HOST_REPLACE_ME/$1/g" .env
 sed -i "s/DB_USER_REPLACE_ME/$2/g" .env
 sed -i "s/DB_PASS_REPLACE_ME/$3/g" .env
+sed -i "s/KEY_REPLACE_ME/$4/g" .env
 npm start &
