ff9aa8719f718439d21f6a29dbb6fa0070edf0dd
francisnazareth <73844173+francisnazareth@users.noreply.github.com>
2022-01-10 21:56:48 +0300

    Update kubernetes-setup.sh

--- kubernetes-setup.sh
+++ kubernetes-setup.sh
@@ -2,7 +2,7 @@
 apt-get update -y
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
